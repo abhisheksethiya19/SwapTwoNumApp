8d024dc3b52eb69c0ea8b2612cea3ed880e575e3
Abhishek <abhisheksethiya1999@gmail.com>
2022-11-10 09:01:29 +0530

    Add project files.

---
+++ SwapTwoNumApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class First
+{
+    static void Main()
+    {
+        string a, b;
+        int temp, c, d;
+        Console.Write("Enter value for a :");
+        a = Console.ReadLine();
+        c = Convert.ToInt32(a);
+        Console.WriteLine("A's value is {0}", c);
+        Console.Write("Enter value for b : ");
+        b = Console.ReadLine();
+        d = Convert.ToInt32(b);
+        Console.WriteLine("B's value is {0}", d);
+        temp = c;
+        c = d;
+        d = temp;
+        Console.WriteLine("Values after swapping are:");
+        Console.WriteLine("a is : " + c);
+        Console.WriteLine("b is : " + d);
+    }
+}
